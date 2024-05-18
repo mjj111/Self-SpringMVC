80db070a5d9acfafc1edf321d06a9ed37148e771
김명준 <86913355+mjj111@users.noreply.github.com>
2024-05-18 21:43:14 +0900

    feat(JsonToUrlEncodedFilter) : body에 들어온 값을 파타미터로 전달할 수 있돌고 필터에서 동작하게 끔 필터 구현 #8

---
+++ src/main/java/spring/web/filter/JsonToUrlEncodedFilter.java
@@ -0,0 +1,24 @@
+package spring.web.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class JsonToUrlEncodedFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if ("POST".equalsIgnoreCase(((HttpServletRequest) request).getMethod())
+                && "application/json".equalsIgnoreCase(request.getContentType())) {
+            request = new JsonToUrlEncodedRequestWrapper(request);
+        }
+        chain.doFilter(request, response);
+    }
+}
