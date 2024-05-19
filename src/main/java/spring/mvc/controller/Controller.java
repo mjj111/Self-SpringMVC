564163d8ad2827766241ce356b771e883c736ad3
김명준 <86913355+mjj111@users.noreply.github.com>
2024-05-19 17:12:52 +0900

    remove : 사용하지 않는 파일 삭제 #13

--- src/main/java/spring/mvc/controller/Controller.java
+++
@@ -1,9 +0,0 @@
-package spring.mvc.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import spring.mvc.view.ModelAndView;
-
-public interface Controller {
-    ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
