ec127f4dfcbc352defb7b00260f99b71d7e5a635
김명준 <86913355+mjj111@users.noreply.github.com>
2024-05-20 15:45:57 +0900

    refactor(UserController) : 비즈니스 로직을 서비스 객체가 갖도록 변경 #15

--- src/main/java/client/controller/UserController.java
+++ src/main/java/client/controller/UserController.java
@@ -1,6 +1,8 @@
 package client.controller;
 
+import client.controller.dto.CreatUserDto;
 import client.model.User;
+import client.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import spring.mvc.annotation.Controller;
@@ -9,21 +11,26 @@ import spring.mvc.controller.AbstractController;
 import spring.mvc.handler.mapping.RequestMethod;
 import spring.mvc.view.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class UserController extends AbstractController {
+
+    private final UserService userService;
+
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping("/users")
     public ModelAndView getUsers(HttpServletRequest request, HttpServletResponse response) {
         int wannaSize = Integer.parseInt(request.getParameter("size"));
+        List<User> users = userService.getUsersWithSize(wannaSize);
 
         ModelAndView modelAndView = jsonView();
         for(int i = 0; i < wannaSize; i++) {
-            User newUser = new User(String.valueOf(i),
-                    "tmpPassword",
-                    i + "김명준",
-                    "skatks1016@gmail.com");
-
-            modelAndView.addObject("user"+ i, newUser);
+            User user = users.get(i);
+            modelAndView.addObject("user"+ i, user);
         }
 
         return modelAndView;
@@ -31,14 +38,11 @@ public class UserController extends AbstractController {
 
     @RequestMapping(value = "/users/create", method = RequestMethod.POST)
     public ModelAndView create(HttpServletRequest request, HttpServletResponse response) {
-        User user = new User(request.getParameter("userId"),
-                request.getParameter("password"),
-                request.getParameter("name"),
-                request.getParameter("email"));
+        CreatUserDto requestDto = CreatUserDto.of(request);
+        User user = userService.createUser(requestDto);
 
         ModelAndView modelAndView = jsonView();
         modelAndView.addObject("user", user);
-
         return modelAndView;
     }
 }
